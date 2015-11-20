4c6498132b2fab57c0c0eab4b022c99497250ced
dv297 <dvv297@gmail.com>
2015-11-20 17:31:55 -0500

    Established set IV

--- app/src/main/java/vuze/twofactorauthentication/MainActivity.java
+++ app/src/main/java/vuze/twofactorauthentication/MainActivity.java
@@ -5,19 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.beardedhen.androidbootstrap.TypefaceProvider;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -27,25 +32,29 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
 
     @Bind(R.id.value_textview)
-    EditText valueTextview;
+    TextView valueTextview;
+    @Bind(R.id.instructions)
+    TextView instructions;
 
     KeyDataSource keySource;
-    @Bind(R.id.scan_button)
-    Button scanButton;
-
-    //    String keyString = "u/Gu5posvwDsXUnV5Zaq4g==";
-    String ivString = "5D9r9ZVzEYYgha93/aUK2w==";
-    AlgorithmParameterSpec iv = new IvParameterSpec(Base64.decode(ivString, Base64.DEFAULT));
-
     SecretKeySpec secretKey;
-    private static byte[] key;
+    byte[] key;
     String cipherType = "AES/CBC/PKCS5Padding";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        TypefaceProvider.registerDefaultIconSets();
+
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            valueTextview.setText(savedInstanceState.getString("keyValue"));
+        }
+
+
         keySource = new KeyDataSource(this);
         keySource.open();
         if (keySource.isKeyPresent()) {
@@ -59,10 +68,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick(R.id.scan_button)
-    public void scanBarcode() {
-        new IntentIntegrator(this).initiateScan();
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("keyValue", valueTextview.getText().toString());
+        super.onSaveInstanceState(savedInstanceState);
     }
+
+        @OnClick(R.id.scan_button)
+    public void scanBarcode() {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setBeepEnabled(false);
+            integrator.initiateScan();
+        }
 
 
     @Override
@@ -74,9 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Log.d(TAG, "Scanned");
-                String plainText = decrypt(result.getContents());
-                Toast.makeText(this, "Scanned: " + plainText, Toast.LENGTH_LONG).show();
-                valueTextview.setText(plainText);
+                String barcodeText = result.getContents();
+                String[] barcodeElements = barcodeText.split(":");
+                String plainText = decrypt(barcodeElements[1], barcodeElements[0]);
+                if(plainText != null) {
+                    valueTextview.setText(plainText);
+                    instructions.setText("Enter the following pin into the extension");
+                }
+
 
             }
         } else {
@@ -85,8 +106,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public String decrypt(String cipherText) {
+                try {
+            Cipher cipher = Cipher.getInstance(cipherType);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            String plainText = (new String(cipher.doFinal(Base64.decode(cipherText, Base64.DEFAULT))));
+            return plainText;
+        } catch (Exception e) {
+            Toast.makeText(this, "Error: " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
+
+    public String decrypt(String cipherText, String ivS) {
+        AlgorithmParameterSpec iv = new IvParameterSpec(Base64.decode(ivS, Base64.DEFAULT));
         try {
             Cipher cipher = Cipher.getInstance(cipherType);
             cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
@@ -109,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return null;
     }
-
 
     public void setKey(String myKey) {
         MessageDigest sha = null;
